64382dd2be4833ffd4bb6432ec0a04ffde9d2ec6
alysahil <106840600+alysahil@users.noreply.github.com>
2024-08-27 16:05:06 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail ="sahil@google.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+let accountState;
+//undefined variables
+// accountId = 2 not allowed
+accountEmail = "hdfc.com"
+accountPassword ="21212121"
+accountCity ="bengaluru"
+
+console.log(accountId);
+/*
+prefer not to use var because of isssue of block and scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
