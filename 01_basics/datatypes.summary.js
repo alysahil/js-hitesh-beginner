99d547f6305e425b8ae9145f303f797910bd5c1a
alysahil <106840600+alysahil@users.noreply.github.com>
2024-08-27 18:38:53 +0000

    data type summary

---
+++ 01_basics/datatypes.summary.js
@@ -0,0 +1,41 @@
+//primitive data types
+//  7 types
+/*
+string
+number
+boolean
+null
+undefined
+symbol
+bigInt
+
+const score =100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id= symbol("123")
+console.log(id===anotherId);
+
+const bigNumber =128838
+
+
+
+//reference type(non primitive)
+
+//array,objects,functions
+
+const heros = ["shaktiman","nagraj","doga"];
+let myObj ={
+name:"hitesh",
+age:22,
+
+}
+const myFunction =function(){
+console.log("hello world");
+
+}
+
+console.log(typeof bigNumber);
